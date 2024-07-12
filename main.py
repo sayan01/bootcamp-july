bf30986fb5a7f98eb08e8197d9ad022eddda6623
Sayan Ghosh <sgic2001@gmail.com>
2024-07-12 10:53:19 +0530

    modularize html and routes.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!' 
+import routes
---
+++ routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from main import app
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!' 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
